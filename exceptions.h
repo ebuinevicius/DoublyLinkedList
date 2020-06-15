7b819d3686a0d7931b228a7e5eee199386bec5c2
Erik Buinevicius <embuinevicius@gmail.com>
2020-06-15 17:33:52 -0500

    Exceptions Header File

---
+++ exceptions.h
@@ -0,0 +1,45 @@
+#ifndef __EXCEPTIONS_H__
+#define __EXCEPTIONS_H__
+
+#include <string>
+#include <iostream>
+
+using namespace std;
+
+class InvalidOperationException {
+private:
+    std::string message;
+public:
+    InvalidOperationException(const char *msg) {
+        message = msg;
+    }
+    InvalidOperationException(const std::string &msg) {
+        message = msg;
+    }
+    std::string getMessage() const {
+        return message;
+    }
+};
+
+class IndexOutOfRangeException {
+private:
+    std::string message;
+    int index;
+public:
+    IndexOutOfRangeException(const char *msg, const int &idx) {
+        message = msg;
+        index = idx;
+    }
+    IndexOutOfRangeException(const std::string &msg, const int &idx) {
+        message = msg;
+        index = idx;
+    }
+    std::string getMessage() const {
+        return message;
+    }
+    int getIndex() const {
+        return index;
+    }
+};
+
+#endif
